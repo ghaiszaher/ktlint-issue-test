c2ae35fa88cfbcc994a609538c16d57db2c6482a
Ghais Zaher <ghaiszaher@gmail.com>
2023-02-26 16:55:49 +0100

    more errors

--- src/main/kotlin/org/example/pkg1/File.kt
+++ src/main/kotlin/org/example/pkg1/File.kt
@@ -2,5 +2,73 @@ package org.example.pkg1
 
 class File {
 
-    fun abc(){}
+    fun f1(){}
+    fun f2(){}
+    fun f3(){}
+    fun f4(){}
+    fun f5(){}
+    fun f6(){}
+    fun f7(){
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+        print("that's a ktlint error"
+        )
+    }
+
 }
